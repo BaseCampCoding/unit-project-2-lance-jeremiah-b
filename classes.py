0ce90975528ceb4beabb967c7f8521fee6f530a3
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-11-09 13:16:58 -0600

    Pushing db and class files.

---
+++ classes.py
@@ -0,0 +1,1 @@
+class ship_classes:
---
+++ db_setup.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+con = sqlite3.connect('Battleship.db')
+
+cur = con.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS Ships(name TEXT, length INTEGER, width INTEGER)')
+
+cur.execute('''INSERT INTO Ships VALUES
+    ('Carrier', 5, 1),
+    ('Battleship', 4, 1),
+    ('Cruiser', 3, 1),
+    ('Submarine', 3, 1),
+    ('Destroyer', 2, 1),
+''')
