057c3cedb3dc31110b2e9409832a87c2f89a3bd2
Lance Easley <leasley21@basecampcodingacademy.org>
2020-11-11 08:20:03 -0600

    Added wait_for_key method

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.1.59"
+        self.server = "192.168.1.64"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
@@ -23,3 +23,9 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
+
+    def wait_for_key(self, key):
+        response = self.client.recv(2048).decode()
+        if response == key:
+            return True
+        return False
