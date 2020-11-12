a0df03685ab79e94924b52120c1a842a083214b5
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-11-12 10:10:14 -0600

    pushing progress in db functions

--- db_setup.py
+++ db_setup.py
@@ -19,28 +19,42 @@ cur = con.cursor()
 # )
 # con.close()
 
-cur.execute("CREATE TABLE IF NOT EXISTS Highscores(scores INTEGER)")
+# cur.execute("CREATE TABLE IF NOT EXISTS Highscores(scores INTEGER)")
+
+def display_highscores():
+    "displays the highscores in a row format"
+    rows = ""
+    cur.execute("SELECT * FROM Highscores ORDER BY scores ASC")
+    for row in cur.fetchall():
+        rows += str(row[0]) + "\n"
+    return rows
 
 winners_score = int(input("num pls: "))
 
-highscore_list = []
-cur.execute("SELECT * FROM Highscores")
-for row in cur.fetchall():
-    highscore_list.append(row[0])
 
-if not winners_score in highscore_list:
-    cur.execute("INSERT INTO Highscores VALUES (?)", (winners_score,))
+def insert_winners_score(int) -> int:
+    "Inserts the winners score if it is valid"
+    highscore_list = []
+    cur.execute("SELECT * FROM Highscores")
+    for row in cur.fetchall():
+        highscore_list.append(row[0])
+    if not winners_score in highscore_list:
+        cur.execute("INSERT INTO Highscores VALUES (?)", (winners_score,))
 
-cur.execute("SELECT * FROM Highscores")
+
+def cur.execute("SELECT * FROM Highscores")
 if len(cur.fetchall()) > 5:
     cur.execute("SELECT * FROM Highscores ORDER BY scores DESC LIMIT 1")
     max_num = cur.fetchall()[0][0]
     cur.execute("DELETE FROM Highscores WHERE scores = ?", (str(max_num),))
 
-con.commit()
+# con.commit()
 
-cur.execute("SELECT * FROM Highscores")
-for row in cur.fetchall():
-    print(row[0])
+
+
+
+insert_winners_score(winners_score)
+print(display_highscores())
+
 
 con.close()
