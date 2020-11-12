9df77945bf45e188008fe46b4ce9886cec1bf2b6
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-11-12 13:07:00 -0600

    pushing progress

--- classes.py
+++ classes.py
@@ -6,3 +6,12 @@ class ship_classes:
         self.name = name
         self.length = length
         self.width = width
+
+    def __str__(self):
+        return f"The {self.name} ship is {self.length} spaces long and {self.width} spaces wide."
+
+    def __repr__(self):
+        return f"{self.name}, {self.length}, {self.width}"
+
+
+Carrier = ship_classes("Carrier", 5, 1)
