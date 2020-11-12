76cf23abdec81127b79805c864ba5d4910ef4b76
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-11-12 10:21:26 -0600

    made funtcions for client file

--- db_setup.py
+++ db_setup.py
@@ -21,6 +21,7 @@ cur = con.cursor()
 
 # cur.execute("CREATE TABLE IF NOT EXISTS Highscores(scores INTEGER)")
 
+
 def display_highscores():
     "displays the highscores in a row format"
     rows = ""
@@ -28,6 +29,7 @@ def display_highscores():
     for row in cur.fetchall():
         rows += str(row[0]) + "\n"
     return rows
+
 
 winners_score = int(input("num pls: "))
 
@@ -40,17 +42,12 @@ def insert_winners_score(int) -> int:
         highscore_list.append(row[0])
     if not winners_score in highscore_list:
         cur.execute("INSERT INTO Highscores VALUES (?)", (winners_score,))
-
-
-def cur.execute("SELECT * FROM Highscores")
-if len(cur.fetchall()) > 5:
-    cur.execute("SELECT * FROM Highscores ORDER BY scores DESC LIMIT 1")
-    max_num = cur.fetchall()[0][0]
-    cur.execute("DELETE FROM Highscores WHERE scores = ?", (str(max_num),))
-
-# con.commit()
-
-
+    cur.execute("SELECT * FROM Highscores")
+    if len(cur.fetchall()) > 5:
+        cur.execute("SELECT * FROM Highscores ORDER BY scores DESC LIMIT 1")
+        max_num = cur.fetchall()[0][0]
+        cur.execute("DELETE FROM Highscores WHERE scores = ?", (str(max_num),))
+    con.commit()
 
 
 insert_winners_score(winners_score)
