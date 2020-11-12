831521f47f1bea7a47afb0684a7b8453b3e34387
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-11-12 12:39:19 -0600

    added some comments.

--- db_setup.py
+++ db_setup.py
@@ -21,7 +21,7 @@ cur.execute(
 cur.execute("CREATE TABLE IF NOT EXISTS Highscores(scores INTEGER)")
 
 
-def reset_highscores():
+def reset_highscores():  # for debugging purposes
     "Resets highscores"
     cur.execute("DELETE FROM Highscores")
     con.commit()
@@ -52,12 +52,15 @@ def insert_winners_score(score: int) -> int:
     25
     30
     """
+    # checks highscore_list to make sure that scores are unique
     highscore_list = []
     cur.execute("SELECT * FROM Highscores")
     for row in cur.fetchall():
         highscore_list.append(row[0])
     if not score in highscore_list:
         cur.execute("INSERT INTO Highscores VALUES (?)", (score,))
+    # makes sure that only the top 5 highscores in the table.
+    # The table will then update the highscore with the lowest moves made, and delete the highest moves made.
     cur.execute("SELECT * FROM Highscores")
     if len(cur.fetchall()) > 5:
         cur.execute("SELECT * FROM Highscores ORDER BY scores DESC LIMIT 1")
